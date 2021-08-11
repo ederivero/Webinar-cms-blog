fee9e7852e40e43ee7099c593432d60847b4dbe1
eduardo de rivero <eduardomanrique@ravn.co>
2021-08-11 14:18:46 -0500

    modificaciones

--- src/controllers/usuario.ts
+++ src/controllers/usuario.ts
@@ -31,6 +31,8 @@ export const registro = async (
       content: jwt,
     });
   } catch (error) {
+    console.log(error);
+
     return res.status(500).json({
       success: false,
       content: error,

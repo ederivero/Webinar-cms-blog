e6573d6b2714766283b0a187d03fc3c871494c94
eduardo de rivero <eduardomanrique@ravn.co>
2021-08-11 16:34:25 -0500

    cors

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,4 +1,10 @@
-import express, { Express, json } from "express";
+import express, {
+  Express,
+  json,
+  NextFunction,
+  Request,
+  Response,
+} from "express";
 import { autorRouter } from "../routes/autor";
 import { postRouter } from "../routes/post";
 import { usuarioRouter } from "../routes/usuario";
@@ -8,8 +14,22 @@ export default class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT || 8000;
+    this.CORS();
     this.bodyParser();
     this.routes();
+  }
+
+  CORS() {
+    this.app.use((req: Request, res: Response, next: NextFunction) => {
+      // Access-Control-Allow-Origin => indicar que origenes (dominios pueden acceder a mi API)
+      res.header("Access-Control-Allow-Origin", "*");
+      // Access-Control-Allow-Headers => indicar que tipos de cabeceras pueden ser enviadas
+      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+      // Access-Control-Allow-Methods => indica que metodos pueden ser intentar acceder a mi backend
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+      // si es que cumple el origen (dominio), el header Y el metodo entonces daremos paso al controlador solicitado
+      next();
+    });
   }
 
   bodyParser() {
